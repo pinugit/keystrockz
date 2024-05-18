df28dc03e69c7844694be5c77256928a4de0587d
pinu <pinuwork@gmail.com>
2024-05-18 15:31:00 +0530

    added favicon and navbar component

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,5 @@
+export default function NavBar() {
+  return (
+    <div className="navbarBlurBg h-[80px] border-b-[1px] border-[#878686] "></div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import NevBarButton from "./components/NavBarButton";
-import ThemeChanger from "./components/ThemeChanger";
-import TypingArea from "./components/TypingComponents/TypingArea";
+import NavBar from "./components/navbar/navbar";
 
 export default function Home() {
-  return (
-    <>
-      <TypingArea />
-      <ThemeChanger />
-      <NevBarButton buttonText={"Typing"} pageLink={"/s"} />
-    </>
-  );
+  return <></>;
 }

9c8bbd1786f39b9086270390b77046130792cf08
pinu <pinuwork@gmail.com>
2024-06-17 15:39:36 -0300

    created refs and stored it for curser to use

--- app/components/TypingComponents/RandomSentenceDisplay.tsx
+++ app/components/TypingComponents/RandomSentenceDisplay.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+"use client";
+import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import RandomLetterObject from "./RandomStringGenerator";
 
 interface props {
@@ -10,17 +11,25 @@ export default function RandomSentenceDisplay({
   lengthOfSentence,
 }: props) {
   const [letterObject, setLetterObject] = useState<string[][]>([[]]);
+  const letterRef = useRef([[null]]);
 
   useEffect(() => {
-    setLetterObject(RandomLetterObject(lengthOfSentence, randomWordList));
-  },[]);
+    const newLetterObject = RandomLetterObject(
+      lengthOfSentence,
+      randomWordList
+    );
+    setLetterObject(newLetterObject);
+    letterRef.current = newLetterObject.map((word) => word.map(() => null));
+  }, []);
 
   return (
     <>
       {letterObject.map((word, index) => (
         <div key={index} className="mr-4 flex ">
           {word.map((letter, indexLetter) => (
-            <div key={indexLetter}>{letter}</div>
+            <div ref={letterRef.current[index][indexLetter]} key={indexLetter}>
+              {letter}
+            </div>
           ))}
         </div>
       ))}
